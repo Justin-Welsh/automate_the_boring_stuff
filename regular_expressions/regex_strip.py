41ebabd23554069be77af1d9f952860e8bce5c95
Justin <jwelsh518@gmail.com>
2020-08-06 20:59:52 -0400

    Added TODOs to regex_strip

--- regular_expressions/regex_strip.py
+++ regular_expressions/regex_strip.py
@@ -5,7 +5,20 @@
 
 import re
 
-def stripping():
-    print("hello!")
+def regex_strip(string, char_to_remove=None):
+    stripping_regex = re.compile(f'{char_to_remove}')
 
-stripping()
+    # TODO: Set up the regex basics
+
+    # TODO: Check to see if the string has any matches (if applicable)
+
+    # TODO: Remove the chars to be removed, as well as whitespace if nothing is provided
+
+
+choice = input("Please enter a string: ")
+to_remove = input("Please enter which chars to remove: ")
+
+if to_remove == '':
+    regex_strip(choice)
+else:
+    regex_strip(choice, to_remove)
