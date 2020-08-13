9e535a53a8fe862f5a9990455a12d4e4be9f420a
Ilyenna <jwelsh518@gmail.com>
2020-08-13 12:02:47 -0400

    Finished solution for sandwichMaker.py

--- input_validation/sandwichMaker.py
+++ input_validation/sandwichMaker.py
@@ -1,9 +1,5 @@
 import pyinputplus as pyip
 
-# TODO: Ask the user if they want mayo, mustard, lettuce, or tomato
-
-# TODO: Ask how many sandwiches they want. Make sure the number is one or more
-
 choiceOfBread = pyip.inputMenu(['wheat', 'white', 'sourdough'])
 choiceOfProtein = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'])
 cheeseOrNot = pyip.inputYesNo(prompt='Would you like cheese?')
@@ -14,4 +10,16 @@ if cheeseOrNot == 'yes':
 else:
     choiceOfCheese = 'no'
 
-print(f'You would like a {choiceOfProtein} sandwich on {choiceOfBread} bread with {choiceOfCheese} cheese.')
+variousCondiments = pyip.inputYesNo(prompt='Would you like mayo, mustard, lettuce, or tomato?')
+# If the user wants condiments
+if variousCondiments == 'yes':
+    choiceOfCondiments = input('Which condiments would you like? ')
+else:
+    choiceOfCondiments = 'no'
+
+numberOfSandwiches = pyip.inputInt(prompt='How many sandwiches would you like?', min=1)
+
+if numberOfSandwiches == 1:
+    print(f'You would like {numberOfSandwiches} {choiceOfProtein} sandwich on {choiceOfBread} bread with {choiceOfCheese} cheese, and {choiceOfCondiments}.')
+else:
+    print(f'You would like {numberOfSandwiches} {choiceOfProtein} sandwiches on {choiceOfBread} bread with {choiceOfCheese} cheese, and {choiceOfCondiments}.')
