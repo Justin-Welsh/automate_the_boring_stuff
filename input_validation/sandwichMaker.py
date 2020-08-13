6c8c697c03cacd9907e5c7bcecf536abdc446c1a
Justin-Welsh <jwelsh518@gmail.com>
2020-08-13 00:17:55 -0400

    Added the beginnings of sandwichMaker.py'

---
+++ input_validation/sandwichMaker.py
@@ -0,0 +1,16 @@
+import pyinputplus as pyip
+
+# TODO: Ask the user if they want mayo, mustard, lettuce, or tomato
+
+# TODO: Ask how many sandwiches they want. Make sure the number is one or more
+
+choiceOfBread = pyip.inputMenu(['wheat', 'white', 'sourdough'])
+choiceOfProtein = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'])
+cheeseOrNot = pyip.inputYesNo(prompt='Would you like cheese?')
+
+if cheeseOrNot == 'yes':
+    choiceOfCheese = pyip.inputMenu(['cheddar', 'swiss', 'mozzarella'])
+else:
+    choiceOfCheese = 'no'
+
+print(f'You would like a {choiceOfProtein} sandwich on {choiceOfBread} bread with {choiceOfCheese} cheese.')
